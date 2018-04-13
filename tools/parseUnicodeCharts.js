cda03c6e9587626e07b508fe7772418ec0f0ef43
Brett Zamir <brettz9@yahoo.com>
2018-04-14 07:36:33 +0800

    - Some work on building Unicode scripts into (column browser) UI

---
+++ tools/parseUnicodeCharts.js
@@ -0,0 +1,62 @@
+/* eslint-env node */
+const fetch = require('node-fetch');
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+
+const fs = require('fs-extra');
+const path = require('path');
+
+(async () => {
+let text;
+const chartsFile = path.join(__dirname, 'unicode-charts.html');
+if (process.argv[2] === 'save') {
+    const resp = await fetch('https://www.unicode.org/charts/');
+    text = await resp.text();
+    console.log('retrieved', text);
+    await fs.writeFile(chartsFile, text);
+    console.log('saved file');
+} else {
+    text = await fs.readFile(chartsFile, 'utf-8');
+}
+
+const doc = new JSDOM(text).window.document;
+const scriptMaps = [...doc.querySelectorAll('table.map')];
+
+scriptMaps.forEach((scriptMap) => {
+    const majorHeading = scriptMap.previousElementSibling.textContent;
+    // const scriptGroups = [...scriptMap.querySelectorAll('table td p')];
+
+    const scriptGroups = [...scriptMap.querySelectorAll('table td p.sg')];
+    // sg, mb, pb/sb
+    const html = `
+    <ul>
+        <li>${majorHeading}</li>
+        ${scriptGroups.map((scriptGroup) =>
+        `<ul>
+            <li>${scriptGroup.textContent}</li>
+            ${
+    (() => {
+        let list = '';
+        do {
+            // TODO: INCOMPLETE/UNTESTED
+            if (scriptGroup.matches('.mb,.pb,.sb')) {
+                list += `<ul>
+                    <li>${scriptGroup.textContent}</li>
+                </ul>`;
+                const oldScriptGroup = scriptGroup;
+                scriptGroup = scriptGroup.nextElementSibling;
+                oldScriptGroup.remove();
+            } else {
+                scriptGroup = scriptGroup.nextElementSibling;
+            }
+        } while (scriptGroup);
+        return list;
+    })()}
+        </ul>`
+    ).join('\n')}
+    </ul>
+`;
+    // console.log('m', majorHeading, scriptGroups);
+    console.log('html', html);
+});
+})();
