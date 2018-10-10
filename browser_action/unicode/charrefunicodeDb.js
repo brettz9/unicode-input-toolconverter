f773b4994285739ec999eb98c164c5c76a6c6cd2
Brett Zamir <brettz9@yahoo.com>
2018-10-10 19:44:15 +0800

    - Styling - Db work

--- browser_action/unicode/charrefunicodeDb.js
+++ browser_action/unicode/charrefunicodeDb.js
@@ -4,41 +4,41 @@
 
 // Todo: Switch to IndexedDB
 class UnicodeDB {
-    constructor () {
-        this.dbConn = null;
-        this.dbConnUnihan = null;
+    constructor ({name, version = 1}) {
+        Object.assign(this, {name, version});
+        this.db = null;
     }
-    connect (dbfile, key) {
-        /*
-        // Todo: Reimplement
-        if (typeof dbfile === 'string' && key !== 'unihan') {
-            const tempfilename = dbfile; // Can have forward slashes for sub-directories
-            // The following is what we normally want to use, but here we can put our data within the extension because
-            //   it should not be edited by the user anyways (whereby they'd want their data to be retained after an update)
-            // dbfile = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('ProfD', Ci.nsILocalFile);
-            // dbfile.append(tempfilename);
-            const MY_ID = 'charrefunicode@brett.zamir';
-            dbfile = FileUtils.getFile(MY_ID, tempfilename);
-        } else {
-            dbfile = FileUtils.getProfFile(dbfile);
-        }
-        if (!dbfile.exists()) { // if it doesn't exist, create  // || !file.isDirectory()
-            throw new Error(`Database ${dbfile.path} doesn't exist`);
-            // dbfile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0777); // DIRECTORY_TYPE
-        }
-        const storageService = Cc['@mozilla.org/storage/service;1'].getService(Ci.mozIStorageService);
-        if (key === 'unihan') {
-            this.dbConnUnihan = storageService.openDatabase(dbfile);
-        } else if (key === 'jamo') {
-            this.dbJamo = storageService.openDatabase(dbfile);
-        } else {
-            this.dbConn = storageService.openDatabase(dbfile);
-        }
-        */
+    upgradeneeded () {
+        throw new Error('`UnicodeDB.upgradeneeded` is an abstract method');
+    }
+    connect () {
+        // Todo: Complete
+        const req = indexedDB.open(this.name, this.version);
+        req.addEventListener('upgradeneeded', (e) => {
+            const {db} = e.target;
+            this.db = db;
+            this.upgradeneeded();
+        });
+        req.addEventListener('success', ({target}) => {
+            this.db = target.result;
+            this.db.addEventListener('onversionchange', () => {});
+        });
+        req.addEventListener('error', () => {});
+        req.addEventListener('blocked', () => {});
     }
 }
 
+export class UnihanDatabase extends UnicodeDB {
+
+}
+
 export class UnicodeDatabase extends UnicodeDB {
+    constructor ({version} = {}) {
+        super({name: 'unicode', version});
+    }
+    upgradeneeded () {
+        this.db.createObjectStore();
+    }
     // Todo: Uncomment and implement
     getUnicodeDescription (entity, currentStartCharCode) {
         // const entityInParentheses = '(' + entity + ') ';
@@ -51,10 +51,17 @@ export class UnicodeDatabase extends UnicodeDB {
 
 // Todo: Reimplement
 export class Jamo extends UnicodeDB {
-    getJamo (code) { // expects decimal string or number
+    constructor ({version} = {}) {
+        super({name: 'jamo', version});
+    }
+    /**
+     * @param {number|string} code // Expects decimal string or number
+     * @returns {string}
+     */
+    getJamo (code) {
         const codePt = typeof code === 'number' ? Math.round(code).toString(16) : code;
         try {
-            const stmt = this.dbConn.dbJamo.createStatement(
+            const stmt = this.db.createStatement(
                 'SELECT `jamo_short_name` FROM Jamo WHERE `code_pt` = "' + codePt.toUpperCase() + '"'
             );
             stmt.executeStep();
@@ -66,12 +73,8 @@ export class Jamo extends UnicodeDB {
 }
 
 const charrefunicodeDb = new UnicodeDatabase();
-
-try {
-    charrefunicodeDb.connect('data/Unicode.sqlite');
-    charrefunicodeDb.connect('Unihan6.sqlite', 'unihan');
-    charrefunicodeDb.connect('data/Jamo.sqlite', 'jamo');
-} catch (e) {
-}
+/*
+const unihanDb = new UnihanDatabase();
+*/
 
 export default charrefunicodeDb;
