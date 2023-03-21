d86c23a67d87046912fa6cd3330fd383095634a7
Brett Zamir <brettz9@yahoo.com>
2023-03-21 01:12:52 -0700

    refactor: remove unused i18n

--- utils/i18n.js
+++
@@ -1,7 +0,0 @@
-export const _ = function _ (...args) {
-  try {
-    return browser.i18n.getMessage(...args);
-  } catch (err) {
-    return `(Non-internationalized string--FIXME!) ${args.join(', ')}`;
-  }
-};
