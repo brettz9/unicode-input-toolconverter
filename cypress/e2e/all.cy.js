01b77f44d6ff46ee024d0ae232bfc3e284caa5ec
Brett Zamir <brettz9@yahoo.com>
2023-03-21 04:06:32 -0700

    test: prefs

--- cypress/e2e/all.cy.js
+++ cypress/e2e/all.cy.js
@@ -1,3 +1,4 @@
 import './about.cy.js';
 import './dtd.cy.js';
 import './index.cy.js';
+import './prefs.cy.js';
---
+++ cypress/e2e/prefs.cy.js
@@ -0,0 +1,19 @@
+import {visitBrowserAction} from './utils.js';
+
+describe('Prefs', function () {
+  it('Resets to defaults', function () {
+    visitBrowserAction();
+    cy.get('h1.tab:nth-of-type(3)').contains('Prefs').click();
+    cy.get('#asciiLt128').should('not.be.checked');
+    cy.get('#asciiLt128').check();
+    cy.get('#asciiLt128').should('be.checked');
+
+    cy.get('#initialTab').should('have.value', 'charts');
+    cy.get('#initialTab').select(1);
+    cy.get('#initialTab').should('have.value', 'conversion');
+
+    cy.get('#resetdefaultbutton').click();
+    cy.get('#asciiLt128').should('not.be.checked');
+    cy.get('#initialTab').should('have.value', 'charts');
+  });
+});
